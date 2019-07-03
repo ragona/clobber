f2041975b463aeb41afd0b89f1772d54b43f4ecf
ragona <ryanragona@gmail.com>
2019-07-03 23:51:07 +0200

    added romio example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(async_await)]
+
+use std::io;
+
+use futures::executor;
+use futures::io::{AsyncReadExt, AllowStdIo};
+
+use romio::TcpStream;
+
+fn main() -> io::Result<()> {
+    executor::block_on(async {
+        let mut stream = TcpStream::connect(&"127.0.0.1:7878".parse().unwrap()).await?;
+        let mut stdout = AllowStdIo::new(io::stdout());
+        stream.copy_into(&mut stdout).await?;
+        Ok(())
+    })
 }
