3221da8f658ae17f05b700e2fa1728a68770fab2
ragona <ryanragona@gmail.com>
2019-07-20 11:22:39 -0700

    working async version

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,42 @@
 
 use std::io;
 use std::thread;
-
-use futures::executor::{self, ThreadPool};
-use futures::io::{AllowStdIo, AsyncReadExt, AsyncWriteExt};
-use futures::task::SpawnExt;
-use futures::StreamExt;
-
-use romio::{TcpListener, TcpStream};
 use std::time::Duration;
+
+use futures::executor;
+use futures::io::{AllowStdIo, AsyncReadExt, AsyncWriteExt};
+
+use romio::TcpStream;
+use juliex;
 
 const HOST: &str = "127.0.0.1:8000";
 const REQUEST: &[u8] = b"GET /";
+const REQUESTS_PER_SECOND: u64 = 1000;
+const TOTAL_REQUESTS: u64 = 10000;
 
 fn main() -> io::Result<()> {
+    let delay = 1e9 as u64 / REQUESTS_PER_SECOND;
+
     executor::block_on(async {
-
-        let mut threadpool = ThreadPool::new().unwrap();
-
-        for i in 0..10 {
-            threadpool
-                .spawn(async move {
-                    let mut stream = TcpStream::connect(&"127.0.0.1:8000".parse().unwrap()).await;
-
-                    match stream {
-                        Ok(mut s) => {
-                            s.write_all(&REQUEST).await;
-                        }
-                        Err(e) => {
-                            dbg!(e);
-                        }
+        for _ in 0..TOTAL_REQUESTS {
+            juliex::spawn(async move {
+                let stream = TcpStream::connect(&HOST.parse().unwrap()).await;
+                match stream {
+                    Ok(mut s) => {
+                        s.write_all(&REQUEST).await.expect("Failed to write to socket");
+                        s.close().await.expect("Failed to close socket");
                     }
-                })
-                .unwrap();
+                    Err(e) => {
+                        eprintln!("{}", e);
+                    }
+                }
+            });
+
+            thread::sleep(Duration::from_nanos(delay));
         }
     });
 
-
-
-//    thread::sleep(Duration::from_millis(500)); // how to block appropriately?
-
     Ok(())
 }
-//    executor::block_on(async {
-//        let mut threadpool = ThreadPool::new()?;
-//
-//        for i in 0..10 {
-//
-//            threadpool
-//                .spawn(async move {
-//                    let mut stream = TcpStream::connect(&HOST.parse().unwrap());
-//                    stream.write(&REQUEST).await?;
-//                })
-//                .unwrap();
-//        }
-//
-//        Ok(())
-//    })
+
+
