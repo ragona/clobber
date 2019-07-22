fc84d0e9b60149ec8be4a58e05a1a923c7aa16c8
ragona <ryanragona@gmail.com>
2019-07-21 22:24:39 -0700

    synchronous threaded version, this one is faster

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,132 @@
-#![feature(async_await)]
-
-use std::io;
+use std::io::prelude::*;
+use std::net::{TcpStream, Ipv4Addr, SocketAddrV4, SocketAddr};
 use std::thread;
-use std::time::Duration;
 
-use futures::executor;
-use futures::io::{AllowStdIo, AsyncReadExt, AsyncWriteExt};
-
-use romio::TcpStream;
-use juliex;
-use futures::lock::Mutex;
-use std::sync::Arc;
+use clap::{App, Arg, ArgMatches};
+use std::time::{Duration, Instant};
+use spin_sleep;
 
 pub mod util;
 
-const HOST: &str = "127.0.0.1:7878";
-const TOTAL_REQUESTS: u64 = 1000000000;
-const REQUESTS_PER_SECOND: u64 = 25000;
-const REQUEST: &[u8] = b"GET / HTTP/1.1
+const REQUEST: &'static [u8] = b"GET / HTTP/1.1
+Host: localhost:8000
 User-Agent: clobber
 Accept: */*\n
 ";
 
 
-fn main() -> io::Result<()> {
-    let delay = 1e9 as u64 / REQUESTS_PER_SECOND;
-    let addr = HOST.parse().expect("Failed to parse host");
+fn cli() -> ArgMatches<'static> {
+    App::new("clobber")
+        .version("0.1")
+        .author("ryan ragona <ryan@ragona.com>")
+        .about("tcp load testing tool")
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .help("Host to clobber")
+                .takes_value(true)
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .help("Port to connect to")
+                .takes_value(true)
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("rate")
+                .short("r")
+                .long("rate")
+                .help("Limit to a particular rate per-second.")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("threads")
+                .long("threads")
+                .help("Number of threads")
+                .takes_value(true)
+        )
+        .get_matches()
+}
 
-    executor::block_on(async {
-        match TcpStream::connect(&addr).await {
-            Ok(mut stream) => {
-                for _ in 0..TOTAL_REQUESTS {
-                    stream.write_all(&REQUEST)
-                        .await
-                        .expect("Failed to write to socket");
+
+fn main() -> std::io::Result<()> {
+    let matches = cli();
+
+    let target = matches
+        .value_of("target")
+        .expect("Failed to parse target")
+        .parse::<Ipv4Addr>()
+        .expect("Failed to parse target");
+
+    let port = matches
+        .value_of("port")
+        .unwrap_or("80")
+        .parse::<u16>()
+        .expect("Failed to parse port");
+
+    let rate = matches
+        .value_of("rate")
+        .unwrap_or("0")
+        .parse::<u64>()
+        .expect("Failed to parse rate");
+
+    let num_threads = matches
+        .value_of("threads")
+        .unwrap_or("4")
+        .parse::<u16>()
+        .expect("Failed to parse number of threads");
+
+
+    // If there is no defined rate, we'll go as fast as we can
+    let delay = match rate {
+        0 => None,
+        _ => {
+            Some(Duration::from_nanos((1e9 as u64 / rate) * num_threads as u64))
+        }
+    };
+
+    let addr:SocketAddr = SocketAddrV4::new(target, port).into();
+
+    let mut thread_handles = vec![];
+
+    for _ in 0..num_threads {
+        thread_handles.push(thread::spawn(move || {
+            // one connection per thread
+            let mut stream = TcpStream::connect(addr).expect("Failed to connect");
+            stream.set_nodelay(true).expect("Failed to set_nodelay");
+
+            loop {
+                // track how long this request takes
+                let start = Instant::now();
+                // write our request
+                match stream.write(REQUEST) {
+                    Ok(_) => (),
+                    // try to reconnect on failure
+                    Err(ref e) if e.kind() == std::io::ErrorKind::BrokenPipe  => {
+                        stream = TcpStream::connect(addr).expect("Failed to reconnect");
+                    }
+                    Err(_) => {
+                        eprintln!("Unexpected error");
+                        break;
+                    }
                 }
 
-                stream.close()
-                    .await
-                    .expect("Failed to close socket");
+                // only try to obey rate limits if we're keeping up with the intended pace
+                let elapsed = Instant::now() - start;
+                if delay.is_some() && elapsed < delay.unwrap() {
+                    spin_sleep::sleep(delay.unwrap() - elapsed);
+                }
             }
-            Err(e) => {
-                eprintln!("Failed to connect to '{}': '{:?}'", &addr, e);
-            }
-        }
-    });
+        }));
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
 
     Ok(())
 }
