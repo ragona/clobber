06426f085aaed5c92ddfcbd7f577fb4000021e04
ragona <ryanragona@gmail.com>
2019-07-21 22:40:10 -0700

    cleaned up main, added cli methods and params, moved networking to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-use std::io::prelude::*;
-use std::net::{TcpStream, Ipv4Addr, SocketAddrV4, SocketAddr};
-use std::thread;
+use std::net::Ipv4Addr;
 
 use clap::{App, Arg, ArgMatches};
-use std::time::{Duration, Instant};
-use spin_sleep;
 
-pub mod util;
-
-const REQUEST: &'static [u8] = b"GET / HTTP/1.1
-Host: localhost:8000
-User-Agent: clobber
-Accept: */*\n
-";
-
+pub mod tcp_client;
 
 fn cli() -> ArgMatches<'static> {
     App::new("clobber")
@@ -26,7 +15,7 @@ fn cli() -> ArgMatches<'static> {
                 .long("target")
                 .help("Host to clobber")
                 .takes_value(true)
-                .required(true)
+                .required(true),
         )
         .arg(
             Arg::with_name("port")
@@ -34,24 +23,30 @@ fn cli() -> ArgMatches<'static> {
                 .long("port")
                 .help("Port to connect to")
                 .takes_value(true)
-                .required(true)
+                .required(true),
         )
         .arg(
             Arg::with_name("rate")
                 .short("r")
                 .long("rate")
                 .help("Limit to a particular rate per-second.")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("threads")
                 .long("threads")
                 .help("Number of threads")
-                .takes_value(true)
+                .takes_value(true),
         )
         .get_matches()
 }
 
+pub struct ClobberSettings {
+    target: Ipv4Addr,
+    port: u16,
+    rate: u64,
+    num_threads: u16,
+}
 
 fn main() -> std::io::Result<()> {
     let matches = cli();
@@ -80,53 +75,12 @@ fn main() -> std::io::Result<()> {
         .parse::<u16>()
         .expect("Failed to parse number of threads");
 
-
-    // If there is no defined rate, we'll go as fast as we can
-    let delay = match rate {
-        0 => None,
-        _ => {
-            Some(Duration::from_nanos((1e9 as u64 / rate) * num_threads as u64))
-        }
-    };
-
-    let addr:SocketAddr = SocketAddrV4::new(target, port).into();
-
-    let mut thread_handles = vec![];
-
-    for _ in 0..num_threads {
-        thread_handles.push(thread::spawn(move || {
-            // one connection per thread
-            let mut stream = TcpStream::connect(addr).expect("Failed to connect");
-            stream.set_nodelay(true).expect("Failed to set_nodelay");
-
-            loop {
-                // track how long this request takes
-                let start = Instant::now();
-                // write our request
-                match stream.write(REQUEST) {
-                    Ok(_) => (),
-                    // try to reconnect on failure
-                    Err(ref e) if e.kind() == std::io::ErrorKind::BrokenPipe  => {
-                        stream = TcpStream::connect(addr).expect("Failed to reconnect");
-                    }
-                    Err(_) => {
-                        eprintln!("Unexpected error");
-                        break;
-                    }
-                }
-
-                // only try to obey rate limits if we're keeping up with the intended pace
-                let elapsed = Instant::now() - start;
-                if delay.is_some() && elapsed < delay.unwrap() {
-                    spin_sleep::sleep(delay.unwrap() - elapsed);
-                }
-            }
-        }));
-    }
-
-    for handle in thread_handles {
-        handle.join().unwrap();
-    }
+    tcp_client::clobber(ClobberSettings {
+        rate,
+        port,
+        target,
+        num_threads,
+    });
 
     Ok(())
 }
---
+++ src/tcp_client.rs
@@ -0,0 +1,61 @@
+use std::io::prelude::*;
+use std::net::{SocketAddr, SocketAddrV4, TcpStream};
+use std::thread;
+use std::time::{Duration, Instant};
+
+use crate::ClobberSettings;
+
+const REQUEST: &'static [u8] = b"GET / HTTP/1.1
+Host: localhost:8000
+User-Agent: clobber
+Accept: */*\n
+";
+
+pub fn clobber(settings: ClobberSettings) {
+    // If there is no defined rate, we'll go as fast as we can
+    let delay = match settings.rate {
+        0 => None,
+        _ => Some(Duration::from_nanos(
+            (1e9 as u64 / settings.rate) * settings.num_threads as u64,
+        )),
+    };
+
+    let addr: SocketAddr = SocketAddrV4::new(settings.target, settings.port).into();
+
+    let mut thread_handles = vec![];
+
+    for _ in 0..settings.num_threads {
+        thread_handles.push(thread::spawn(move || {
+            // one connection per thread
+            let mut stream = TcpStream::connect(addr).expect("Failed to connect");
+            stream.set_nodelay(true).expect("Failed to set_nodelay");
+
+            loop {
+                // track how long this request takes
+                let start = Instant::now();
+                // write our request
+                match stream.write(REQUEST) {
+                    Ok(_) => (),
+                    // some clients break the pipe after each request
+                    Err(ref e) if e.kind() == std::io::ErrorKind::BrokenPipe => {
+                        stream = TcpStream::connect(addr).expect("Failed to reconnect");
+                    }
+                    Err(_) => {
+                        eprintln!("Unexpected error");
+                        break;
+                    }
+                }
+
+                // only try to obey rate limits if we're keeping up with the intended pace
+                let elapsed = Instant::now() - start;
+                if delay.is_some() && elapsed < delay.unwrap() {
+                    spin_sleep::sleep(delay.unwrap() - elapsed);
+                }
+            }
+        }));
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
+}
