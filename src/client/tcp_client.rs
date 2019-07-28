bb80dc9fe7d28e6cc948ad2edab0bd693d97da16
ragona <ryanragona@gmail.com>
2019-07-28 14:50:32 -0700

    code readability refactor

--- src/client/tcp_client.rs
+++ src/client/tcp_client.rs
@@ -51,68 +51,20 @@ up with more than its fair share of poor connections, we'll see if that's necess
 **/
 
 pub fn clobber(settings: ClobberSettings, message: Message, close: Receiver<()>) -> Result<()> {
-    let delay = match settings.rate {
-        0 => None,
-        _ => Some(Duration::from_nanos(1e9 as u64 / settings.rate) * settings.num_threads as u32),
-    };
-
     let addr: SocketAddr = SocketAddrV4::new(settings.target, settings.port).into();
     let mut thread_handles: Vec<JoinHandle<Result<()>>> = vec![];
 
     for _ in 0..settings.num_threads {
+        // todo: figure out why I can't clone these inline with the function invocation
         let msg = message.clone();
-        let rcv = close.clone();
-        let mut thread_metrics = Metrics::new();
-
-        // sleep here to stagger worker threads
-        spin_sleep::sleep(Duration::from_millis(1000 / settings.num_threads as u64));
+        let cls = close.clone();
 
         thread_handles.push(thread::spawn(move || {
-            // todo: don't panic on connection failure
-            let mut stream = connect_and_configure(addr)?;
-            let mut read_buf = [0u8; 65535];
-
-            while !close_requested(&rcv) {
-                let start = Instant::now();
-                // todo: Handle partial writes
-                match stream.write(msg.bytes.as_slice()) {
-                    Err(e) => {
-                        error!("Unexpected error: {}", e);
-                        break;
-                    }
-                    Ok(x) => {
-                        trace!("write complete {}", x);
-                        thread_metrics.requests += 1;
-                    }
-                }
-
-                loop {
-                    match stream.read(&mut read_buf) {
-                        Ok(0) => break,
-                        Ok(n) => trace!("read {} bytes", n),
-                        Err(e) => {
-                            warn!("{}", e);
-                            break;
-                        }
-                    }
-                }
-
-                // only try to obey rate limits if we're keeping up with the intended pace
-                let elapsed = Instant::now() - start;
-                match delay {
-                    Some(delay) => {
-                        if elapsed < delay {
-                            spin_sleep::sleep(delay - elapsed);
-                        }
-                    }
-                    None => {}
-                }
-            }
-
-            info!("{:?}", thread_metrics);
-
-            Ok(())
+            sub_clobber(addr.clone(), msg, cls, settings.clone())
         }));
+
+        // try to evenly stagger thread starts
+        spin_sleep::sleep(Duration::from_millis(1000 / settings.num_threads as u64));
     }
 
     for handle in thread_handles {
@@ -121,6 +73,53 @@ pub fn clobber(settings: ClobberSettings, message: Message, close: Receiver<()>)
             .expect("Failed to join on child thread")
             .expect("Child thread failed to return");
     }
+
+    Ok(())
+}
+
+// todo: turn this into a struct with an implementation
+fn sub_clobber(
+    addr: SocketAddr,
+    message: Message,
+    close: Receiver<()>,
+    settings: ClobberSettings,
+) -> Result<()> {
+    let mut stream = connect_and_configure(addr)?;
+    let mut read_buf = [0u8; 65535];
+    let delay = match settings.rate {
+        0 => None,
+        _ => Some(Duration::from_nanos(1e9 as u64 / settings.rate) * settings.num_threads as u32),
+    };
+
+    while !close_requested(&close) {
+        let start = Instant::now();
+
+        // todo: Handle partial writes
+        match stream.write(message.bytes.as_slice()) {
+            Err(e) => {
+                error!("Unexpected error: {}", e);
+                break;
+            }
+            Ok(x) => {
+                trace!("write complete {}", x);
+            }
+        }
+
+        loop {
+            match stream.read(&mut read_buf) {
+                Ok(0) => break,
+                Ok(n) => trace!("read {} bytes", n),
+                Err(e) => {
+                    warn!("{}", e);
+                    break;
+                }
+            }
+        }
+
+        limit_rate(&start, &delay);
+    }
+
+    info!("child thread closed");
 
     Ok(())
 }
@@ -138,5 +137,17 @@ fn close_requested(close: &Receiver<()>) -> bool {
     match close.try_recv() {
         Ok(_) | Err(TryRecvError::Disconnected) => true,
         Err(TryRecvError::Empty) => false,
+    }
+}
+
+fn limit_rate(start: &Instant, delay: &Option<Duration>) {
+    let elapsed = Instant::now() - *start;
+    match delay {
+        Some(delay) => {
+            if elapsed < *delay {
+                spin_sleep::sleep(*delay - elapsed);
+            }
+        }
+        None => {}
     }
 }
