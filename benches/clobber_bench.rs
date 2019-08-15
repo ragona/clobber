75b85afb9884ef8b394ad221bd67ed5bc81f935a
ragona <ryanragona@gmail.com>
2019-08-14 22:27:59 -0700

    added a quick benchmark

---
+++ benches/clobber_bench.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::black_box;
+use criterion::Criterion;
+
+use clobber::{tcp, util::test_server, Config, Message};
+
+fn clobber(n: u32) {
+    let (addr, _receiver) = test_server();
+    let config = Config {
+        target: addr,
+        connections: 10,
+        rate: None,
+        limit: Some(n),
+        duration: None,
+        num_threads: Some(1),
+        connect_timeout: None,
+        read_timeout: None,
+    };
+    let message = Message::new(b"GET / HTTP/1.1\r\nHost: localhost:8000\r\n\r\n".to_vec());
+
+    tcp::clobber(config, message).unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("clobber 250", |b| b.iter(|| clobber(black_box(250))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,46 @@
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::net::SocketAddr;
+use std::time::Duration;
 
-use rand::Rng;
+use futures::executor;
+use futures::prelude::*;
 
-pub fn random_ipv4_addr(port: u16) -> SocketAddr {
-    let random_bytes = rand::thread_rng().gen::<[u8; 4]>();
-    let ip = Ipv4Addr::new(
-        random_bytes[0],
-        random_bytes[1],
-        random_bytes[2],
-        random_bytes[3],
-    );
+use crate::{tcp, Config, Message, Stats};
+use crossbeam_channel::Receiver;
 
-    SocketAddrV4::new(ip, port).into()
+/// Echo server for testing
+/// todo: Allow tests to pass in an enum to configure how the server behaves. (e.g. Echo vs. static.)
+pub fn test_server() -> (SocketAddr, Receiver<Stats>) {
+    let mut server = romio::TcpListener::bind(&"127.0.0.1:0".parse().unwrap()).unwrap();
+    let mut read_buf = [0u8; 128];
+    let addr = server.local_addr().unwrap();
+    let (tx, rx) = crossbeam_channel::unbounded();
+
+    std::thread::spawn(move || {
+        executor::block_on(async move {
+            let mut incoming = server.incoming();
+            while let Some(stream) = incoming.next().await {
+                match stream {
+                    Ok(mut stream) => {
+                        let tx = tx.clone();
+                        let mut stats = Stats::new();
+                        stats.connections += 1;
+                        juliex::spawn(async move {
+                            stream.read(&mut read_buf).await.unwrap();
+                            stats.bytes_read += read_buf.len();
+                            stream.write(&read_buf).await.unwrap();
+                            stats.bytes_written += read_buf.len();
+                            stream.close().await.unwrap();
+
+                            tx.send(stats).unwrap();
+                        });
+                    }
+                    Err(e) => {
+                        panic!(e);
+                    }
+                }
+            }
+        })
+    });
+
+    (addr, rx)
 }
