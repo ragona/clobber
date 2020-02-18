53ed346e77e66a9f50e3696331a91b170a632c5b
ragona <ryanragona@gmail.com>
2020-02-17 17:01:41 -0800

    moved logic out of main

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use futures::prelude::*;
+use std::time::Duration;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+
 #[derive(Debug)]
 pub struct Work {
     cur: usize,
@@ -38,6 +42,65 @@ impl Iterator for Work {
             _ => None,
         }
     }
+}
+
+pub async fn go(worker_count: usize) -> Result<(), Box<dyn std::error::Error>> {
+    let jobs = vec![Work::new(10), Work::new(10)];
+    let (task_tx, task_rx) = channel(10);
+    
+    tokio::spawn(async move {
+        generate_work(task_tx, jobs).await;
+    });
+
+    work(task_rx, worker_count).await;
+
+    Ok(())
+}
+
+async fn generate_work(mut task_tx: Sender<Task>, mut jobs: Vec<Work>) {
+    loop {
+        // todo: receive results, reorder jobs on priority (or make jobs a heap)
+
+        if !jobs.is_empty() {
+            let job = jobs.last_mut().unwrap();
+            match job.next() {
+                Some(task) => {
+                    if let Err(_) = task_tx.send(task).await {
+                        println!("we broke");
+                        break;
+                    }
+                }
+                None => {
+                    jobs.pop().unwrap();
+                }
+            }
+        } else {
+            // todo: This is sort of a race condition where if new work doesn't
+            // show up before all potential new work is found from outstanding 
+            // requests we'll drop out too early.
+            break;
+        }
+    }
+}
+
+async fn work(task_rx: Receiver<Task>, worker_count: usize) {
+    task_rx
+        .map(|task| {
+            async move {
+                // pretend to do work
+                tokio::time::delay_for(Duration::from_millis(100)).await;
+
+                // return some results
+                Output { val: task.0 }
+            }
+        })
+        .buffered(worker_count)
+        .for_each(|out| {
+            async move {
+                println!("completed {}", out.val)
+            }
+        })
+        .await;
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,7 @@
-use clobber::{Output, Task, Work};
-use futures::prelude::*;
-use std::time::Duration;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
-
-const NUM_WORKERS: usize = 8;
-
+use clobber;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let jobs = vec![Work::new(10), Work::new(10)];
-    let (task_tx, task_rx) = channel(10);
+    clobber::go(8).await?;
     
-    tokio::spawn(async move {
-        generate_work(task_tx, jobs).await;
-    });
-
-    work(task_rx).await;
-
     Ok(())
-}
-
-async fn generate_work(mut task_tx: Sender<Task>, mut jobs: Vec<Work>) {
-    loop {
-        // todo: receive results, reorder jobs on priority (or make jobs a heap)
-
-        if !jobs.is_empty() {
-            let job = jobs.last_mut().unwrap();
-            match job.next() {
-                Some(task) => {
-                    if let Err(_) = task_tx.send(task).await {
-                        println!("we broke");
-                        break;
-                    }
-                }
-                None => {
-                    jobs.pop().unwrap();
-                }
-            }
-        } else {
-            // todo: This is sort of a race condition where if new work doesn't
-            // show up before all potential new work is found from outstanding 
-            // requests we'll drop out too early.
-            break;
-        }
-    }
-}
-
-async fn work(task_rx: Receiver<Task>) {
-    task_rx
-        .map(|task| {
-            async move {
-                // pretend to do work
-                tokio::time::delay_for(Duration::from_millis(100)).await;
-
-                // return some results
-                Output { val: task.0 }
-            }
-        })
-        .buffered(NUM_WORKERS)
-        .for_each(|out| {
-            async move {
-                println!("completed {}", out.val)
-            }
-        })
-        .await;
 }
