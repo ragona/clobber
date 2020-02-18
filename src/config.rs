b307576563e130917184d05af07d20045a87c9eb
ragona <ryanragona@gmail.com>
2020-02-17 17:17:56 -0800

    Non functional but error-free merge of old branch with async-std removed

---
+++ src/config.rs
@@ -0,0 +1,164 @@
+//! Settings that control the test
+//!
+//! This is a struct and impl that contains the configuration options. `Config` should be easy
+//! to copy, as it is passed to each `connection()` call.
+//!
+
+use std::net::SocketAddr;
+use std::time::Duration;
+
+use serde_derive::Deserialize;
+
+/// Settings for the load test
+///
+/// todo: Make write/read optional. (Enum?)
+///
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    /// Socket address (ip and port) of the host we'll be calling
+    pub target: SocketAddr,
+    /// Connections is the a key knob to turn when tuning a performance test. Honestly
+    /// 'connections' isn't the best name; it implies a certain persistance that isn't true.
+    /// todo: Rename to workers?
+    pub connections: u32,
+    /// Optional rate-limiting. Precisely timing high rates is unreliable; if you're
+    /// seeing slower than expected performance try running with no rate limit at all.
+    pub rate: Option<u32>,
+    /// Optional duration. If duration is None, clobber will run indefinitely.
+    pub duration: Option<Duration>,
+    /// Number of OS threads to distribute work between. 0 becomes num_cpus.
+    pub threads: Option<u32>,
+    /// Optionally time out requests at a number of milliseconds. Note: checking timeouts
+    /// costs CPU cycles; max performance will suffer. However, if you have an ill-behaving
+    /// server that isn't connecting consistently and is hanging onto connections, this can
+    /// improve the situation.
+    pub connect_timeout: Option<u32>,
+    /// Optionally time out read requests at a number of milliseconds. Note: checking timeouts
+    /// costs CPU cycles; max performance will suffer. However, if you have an ill-behaving server
+    /// that isn't sending EOF bytes or otherwise isn't dropping connections, this can be
+    /// essential to maintaing a high(ish) throughput, at the cost of more CPU load.
+    pub read_timeout: Option<u32>,
+    /// Absolute number of requests to be made. Should split evenly across threads.
+    pub limit: Option<u32>,
+    /// Repeats the outgoing message multiple times per connection.
+    pub repeat: u32,
+    /// Fuzzing config toml file
+    pub fuzz_path: Option<String>,
+}
+
+impl Config {
+    pub fn new(target: SocketAddr) -> Config {
+        let connections = 100; // todo: detect?
+        Config {
+            target,
+            connections,
+            rate: None,
+            limit: None,
+            repeat: 1,
+            duration: None,
+            threads: None,
+            read_timeout: None,
+            connect_timeout: None,
+            fuzz_path: None,
+        }
+    }
+
+    /// Number of user-defined threads, or all the threads on the host.
+    pub fn num_threads(&self) -> u32 {
+        match self.threads {
+            None => num_cpus::get() as u32,
+            Some(n) => n,
+        }
+    }
+
+    /// Number of connection loops each thread should maintain. Will never be less than the number
+    /// of threads.
+    pub fn connections_per_thread(&self) -> u32 {
+        match self.connections / self.num_threads() as u32 {
+            0 => 1,
+            n => n,
+        }
+    }
+
+    /// The amount a single connection should wait between loops in order to maintain the defined
+    /// rate. Returns a default duration if there is no rate.
+    pub fn connection_delay(&self) -> Duration {
+        match self.rate {
+            Some(rate) => {
+                Duration::from_secs(1) / rate * self.connections_per_thread() * self.num_threads()
+            }
+            None => Duration::default(),
+        }
+    }
+
+    /// The number of iterations each connection loop should perform before stopping. Doesn't play
+    /// nice with limits that are not divisible by the number of threads and connections.
+    pub fn limit_per_connection(&self) -> Option<u32> {
+        match self.limit {
+            None => None,
+            Some(n) => Some(n / self.connections),
+        }
+    }
+}
+
+pub struct ConfigBuilder {
+    config: Config,
+}
+
+// todo: Oof, this should be a macro
+impl ConfigBuilder {
+    pub fn new(target: SocketAddr) -> ConfigBuilder {
+        ConfigBuilder {
+            config: Config::new(target),
+        }
+    }
+
+    pub fn build(self) -> Config {
+        self.config
+    }
+
+    pub fn connections(mut self, connections: u32) -> ConfigBuilder {
+        self.config.connections = connections;
+        self
+    }
+
+    pub fn rate(mut self, rate: Option<u32>) -> ConfigBuilder {
+        self.config.rate = rate;
+        self
+    }
+
+    pub fn duration(mut self, duration: Option<Duration>) -> ConfigBuilder {
+        self.config.duration = duration;
+        self
+    }
+
+    pub fn threads(mut self, threads: Option<u32>) -> ConfigBuilder {
+        self.config.threads = threads;
+        self
+    }
+
+    pub fn connect_timeout(mut self, connect_timeout: Option<u32>) -> ConfigBuilder {
+        self.config.connect_timeout = connect_timeout;
+        self
+    }
+
+    pub fn read_timeout(mut self, read_timeout: Option<u32>) -> ConfigBuilder {
+        self.config.read_timeout = read_timeout;
+        self
+    }
+
+    pub fn limit(mut self, limit: Option<u32>) -> ConfigBuilder {
+        self.config.limit = limit;
+        self
+    }
+
+    pub fn repeat(mut self, repeat: u32) -> ConfigBuilder {
+        self.config.repeat = repeat;
+        self
+    }
+
+    pub fn fuzz_path(mut self, path: Option<String>) -> ConfigBuilder {
+        self.config.fuzz_path = path;
+        self
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,190 @@
-use clobber;
+use std::net::SocketAddr;
+
+use clap::{App, Arg, ArgMatches};
+use humantime;
+
+use clobber::util::optional_stdin;
+use clobber::{self, setup_logger, Config, ConfigBuilder};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    clobber::go(8).await?;
+    let cli = cli();
+    let matches = cli.get_matches();
+    let settings = settings_from_argmatches(&matches);
+
+    let log_level = match &matches.occurrences_of("v") {
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
+        3 => log::LevelFilter::Trace,
+        _ => log::LevelFilter::Warn,
+    };
+
+    setup_logger(log_level).expect("Failed to setup logger");
+
+    let bytes = match optional_stdin() {
+        Some(bytes) => bytes,
+        None => unimplemented!("no request body"), // todo: Load from file
+    };
     
+    clobber::go(8).await?;
+
     Ok(())
+}
+
+fn cli() -> App<'static, 'static> {
+    App::new("clobber")
+        .version("0.1")
+        .about("tcp load testing tool")
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .help("Host to clobber")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("rate")
+                .long("rate")
+                .help("Limit to a particular rate per-second.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("threads")
+                .long("threads")
+                .help("Number of threads")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the log level, from -v to -vvv"),
+        )
+        .arg(
+            Arg::with_name("connections")
+                .short("c")
+                .long("connections")
+                .help("Max number of open connections at any given time")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("duration")
+                .short("d")
+                .long("duration")
+                .help("Length of the run (e.g. 5s, 10m, 2h, etc...)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("connect-timeout")
+                .long("connect-timeout")
+                .help("Timeout for initial TCP syn timeout")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("read-timeout")
+                .long("read-timeout")
+                .help("Timeout for reading response from target")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("limit")
+                .short("l")
+                .long("limit")
+                .help("Total number of requests")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("repeat")
+                .short("r")
+                .long("repeat")
+                .help("Repeats the outgoing message")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("fuzz")
+                .short("f")
+                .long("fuzz")
+                .help("Path to a fuzzing config .toml file")
+                .takes_value(true),
+        )
+}
+
+fn settings_from_argmatches(matches: &ArgMatches) -> Config {
+    let target = matches
+        .value_of("target")
+        .unwrap()
+        .parse::<SocketAddr>()
+        .expect("Failed to parse target");
+
+    let rate = matches
+        .value_of("rate")
+        .unwrap_or("0")
+        .parse::<u32>()
+        .expect("Failed to parse rate");
+
+    let repeat = matches
+        .value_of("repeat")
+        .unwrap_or("1")
+        .parse::<u32>()
+        .expect("Failed to parse repeat");
+
+    let limit = matches
+        .value_of("limit")
+        .unwrap_or("0")
+        .parse::<u32>()
+        .expect("Failed to parse limit");
+
+    let connections = matches
+        .value_of("connections")
+        .unwrap_or("100")
+        .parse::<u32>()
+        .expect("Failed to parse connections");
+
+    let fuzz_path = match matches.value_of("fuzz") {
+        None => None,
+        Some(s) => Some(String::from(s)),
+    };
+
+    let connect_timeout = match matches.value_of("connect-timeout") {
+        Some(timeout) => Some(timeout.parse().expect("Failed to parse connect_timeout")),
+        None => None,
+    };
+
+    let read_timeout = match matches.value_of("read-timeout") {
+        Some(timeout) => Some(timeout.parse().expect("Failed to parse read_timeout")),
+        None => None,
+    };
+
+    let threads = match matches.value_of("threads") {
+        Some(threads) => Some(threads.parse::<u32>().expect("Failed to parse threads")),
+        None => None,
+    };
+
+    let duration = match matches.value_of("duration") {
+        Some(s) => Some(humantime::parse_duration(s).expect("Failed to parse duration")),
+        None => None,
+    };
+
+    let limit = match limit {
+        0 => None,
+        n => Some(n),
+    };
+
+    let rate = match rate {
+        0 => None,
+        n => Some(n),
+    };
+
+    ConfigBuilder::new(target)
+        .rate(rate)
+        .limit(limit)
+        .repeat(repeat)
+        .threads(threads)
+        .duration(duration)
+        .fuzz_path(fuzz_path)
+        .connections(connections)
+        .read_timeout(read_timeout)
+        .connect_timeout(connect_timeout)
+        .build()
 }
