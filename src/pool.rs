586c9e1a869e8c592486924aa2025576ab7f33d2
Ryan Ragona <ryanragona@gmail.com>
2020-07-04 10:50:04 -0700

    adding workerpool stubs

--- src/pool.rs
+++ src/pool.rs
@@ -1,19 +1,48 @@
 use async_std::prelude::*;
-use crossbeam_channel::{unbounded, Receiver};
+use async_std::sync::{channel, Receiver, Sender};
+use std::collections::VecDeque;
 
 struct Worker<In, Out, F: Future<Output = Out>> {
     job: fn(In) -> F,
+    incoming: Receiver<Sender<Out>>,
 }
 
 impl<In, Out, F> Worker<In, Out, F>
 where
     F: Future<Output = Out>,
+    In: Send + Sync,
+    Out: Send + Sync,
 {
-    pub async fn work(self, input: In) -> Out {
+    async fn work(self, input: In) -> Out {
         (self.job)(input).await
+    }
+
+    pub fn new(job: fn(In) -> F, incoming: Receiver<Sender<Out>>) -> Self {
+        Self { job, incoming }
     }
 }
 
+struct WorkerPool<In, Out, F>
+where
+    F: Future<Output = Out>,
+{
+    incoming: VecDeque<In>,
+    task: fn(In) -> F,
+}
+
+impl<In, Out, F> WorkerPool<In, Out, F>
+where
+    In: Send + Sync,
+    Out: Send + Sync,
+    F: Future<Output = Out>,
+{
+    pub fn new(task: fn(In) -> F, max_workers: usize) -> Self {
+        Self {
+            incoming: VecDeque::with_capacity(max_workers),
+            task,
+        }
+    }
+}
 async fn double(x: usize) -> usize {
     x * 2
 }
@@ -25,7 +54,12 @@ mod tests {
 
     #[async_test]
     async fn single_worker() {
-        let doubler = Worker { job: double };
+        let (send, recv) = channel(1);
+        let doubler = Worker {
+            job: double,
+            incoming: recv,
+        };
+
         assert_eq!(doubler.work(10).await, 20);
     }
 }
