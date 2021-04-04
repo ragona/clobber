233fc86553bb665c39aa215f0d6a0b031899cf74
Ryan Ragona <ryanragona@gmail.com>
2021-04-04 10:50:18 -0700

    many

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 use std::error::Error;
 use std::time::{Duration, Instant};
 
+use futures::stream::{FuturesUnordered, StreamExt};
 use hyper::Client;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let end = Instant::now() + Duration::from_secs(5);
-    let client = Client::new();
-    let mut i = 0_usize;
+const TEST_LENGTH_SEC: u64 = 5;
+const NUM_WORKERS: u64 = 24;
 
-    while Instant::now() < end {
-        client.get("http://localhost:8080".parse().unwrap()).await?;
-        i += 1;
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut workers = FuturesUnordered::new();
+    for _ in 0..NUM_WORKERS {
+        workers.push(tokio::spawn(async { worker().await.unwrap() }))
     }
 
-    println!("{}/s", i / 5);
+    let mut total_requests = 0;
+    while let Some(count) = workers.next().await {
+        total_requests += count?;
+    }
 
+    println!("{}/s", total_requests / TEST_LENGTH_SEC);
     Ok(())
+}
+
+async fn worker() -> Result<u64> {
+    let mut count = 0;
+
+    let client = Client::new();
+    let end = Instant::now() + Duration::from_secs(TEST_LENGTH_SEC);
+
+    while Instant::now() < end {
+        // client.get("http://localhost:8080".parse().unwrap()).await?;
+        count += 1;
+    }
+
+    Ok(count)
 }
